91ce8e76044337e434e6a41b7ea34f57cc0f8806
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-08-18 20:44:44 +0300

    .

--- Tasks 1.2/src/Task2.java
+++ Tasks 1.2/src/Task2.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the lenght of the first leg of the triangle:");
+        double leg1 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Give the lenght of the second leg of the triangle:");
+        double leg2 = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("The length of the hypotenuse is:" + Math.sqrt(leg1*leg1 + leg2*leg2));
+        scanner.close();
+    }
 }
--- Tasks 1.2/src/Task3.java
+++ Tasks 1.2/src/Task3.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give the amount of grams");
+        double grams = Double.parseDouble(scanner.nextLine());
+        double leiviska = grams / 13.28 / 32 / 20;
+        double naula = leiviska*20%20;
+        double luoti = naula*32%32;
+        System.out.printf("The grams equal to %.0f",leiviska);
+        System.out.printf(" leiviskä, %.0f",naula);
+        System.out.printf(" naula and %.2f",luoti);
+        System.out.print(" luoti." );
+        scanner.close();
+    }
 }
