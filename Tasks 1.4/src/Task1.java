61515169cdbeb09fdae92671523bbd065798f55b
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-08-19 13:55:28 +0300

    Tasks 1.4 Task1

--- Tasks 1.4/src/Task1.java
+++ Tasks 1.4/src/Task1.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String x;
+        Scanner input = new Scanner(System.in);
 
-    }
-}
+        System.out.println("Generate name? Y/N");
+        x=input.nextLine().toLowerCase(Locale.getDefault());
+
+        if (x.equals("n")) System.exit(1);
+
+        do{
+            String[] fnames= {"Eero","Lucas","Juhani","Jarkko","Jaakko","Kari","Jyri","Jari","Sampo","Aapo","Salainen"};
+            String[] lnames= {"Koivukoski","Mattinen","Mäntykoski","Jokilainen","Kantolainen","Kärkkäinen","Harjulainen","Froidilainen","Salainen"};
+            System.out.println( fnames[(int)(Math.random()*11)]+ " " + lnames[(int)(Math.random()*9)]);
+            System.out.println("Generate name? Y/N");
+            x=input.nextLine().toLowerCase(Locale.getDefault());
+        }while(!x.equals("n"));
+}}
