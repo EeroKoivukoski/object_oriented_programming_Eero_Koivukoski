8cd914cadb55ab3db3456bd697a0e9647fb61977
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-08-21 09:05:37 +0300

    Tasks 1.4 Task2 refinement

--- Tasks 1.4/src/Task2.java
+++ Tasks 1.4/src/Task2.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        //Create a array which the user will enter all wanted integers
         System.out.println("How many integers do you want to enter?");
         int num = input.nextInt();
         int[] numbers = new int[num];
@@ -10,20 +12,31 @@ public class Task2 {
             System.out.print("Please enter number " + i + ": ");
             numbers[i-1] = input.nextInt();
         }
-        int hsum=0,end=0,start = 0,sum;
+
+        //Create variable to save the highest sum and the end and start numbers of the max sum
+        int hsum=0,sum;
+        String start_end = " ";
         boolean initial = true;
-        for (int j = 1; j <= num; j++) {
+
+        // The first loop goes through each number in the array
+        for (int currentstart = 1; currentstart <= num; currentstart++) {
+
+            // create the sum variable to check each group of possible sums
             sum = 0;
-            for (int i = j; i <= num; i++) {
-                sum += numbers[i-1];
+
+            // Second loop adds each nuber in the array one at a time
+            for (int currentend = currentstart; currentend <= num; currentend++) {
+                sum += numbers[currentend-1];
+
+                // If the current sum is larger or equal than the current max sum, the max sum gets replaced by the current sum.
+                // When the max sum gets saved the start and end of the current sum is saved as well.
                 if (sum >= hsum || initial) {
                     initial = false;
                     hsum = sum;
-                    start = j;
-                    end = i;
+                    start_end = " "+currentstart +"-"+ currentend+".";
                 }
             }
         }
-        System.out.println("Largest sum possible is: "+ hsum + "\nIntegers: "+ start+"-"+end+".");
+        System.out.println("Largest sum possible is: "+ hsum + "\nIntegers:"+ start_end+".");
     }
 }
