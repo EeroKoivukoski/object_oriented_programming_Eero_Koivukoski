23cec4520efdd21a1fad707adc1b82ed14069087
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-08-25 09:47:41 +0300

    Tasks 2.2 Task1 improvement

--- Tasks 2.2/src/Task1.java
+++ Tasks 2.2/src/Task1.java
@@ -3,21 +3,28 @@ import java.util.ArrayList;
 class GroceryListManager {
     private final ArrayList<String> groceryList = new ArrayList<>();
 
-    void addItem(String item){ groceryList.add(item); }
-
-    void removeItem(String item){ groceryList.remove(item); }
+    void addItem(String item){
+        System.out.println("Adding " + item + " to the list...");
+        groceryList.add(item);
+    }
+    void removeItem(String item){
+        System.out.println("Removing " + item + " from the list...");
+        groceryList.remove(item);
+    }
 
     void displayList(){
+        int itemCount = 1;
+        System.out.println("Grocery List:");
         for(String item : groceryList){
-            System.out.println(item);
+            System.out.println(itemCount++ + ". " + item);
         }
     }
     void checkItem(String item){
         if(groceryList.contains(item)){
-            System.out.println("Item is in the list!");
+            System.out.println(item + " is in the list!");
         }
         else{
-            System.out.println("Item is not in the list.");
+            System.out.println(item + " is not in the list.");
         }
     }
 }
@@ -25,15 +32,15 @@ class GroceryListManager {
 class GroceryListTester  {
     public static void main(String[] args) {
         GroceryListManager myGroceryListManager      = new GroceryListManager();
-        myGroceryListManager.addItem("Tomato");
-        myGroceryListManager.addItem("Pepsi");
-        myGroceryListManager.addItem("Cheese");
-        myGroceryListManager.addItem("Chicken");
+        myGroceryListManager.addItem("Tomato ketchup");
+        myGroceryListManager.addItem("Pepsi cola");
+        myGroceryListManager.addItem("Halloumi cheese");
+        myGroceryListManager.addItem("Chicken nugget");
         myGroceryListManager.displayList();
-        myGroceryListManager.removeItem("Tomato");
-        myGroceryListManager.removeItem("Pepsi");
+        myGroceryListManager.removeItem("Tomato ketchup");
+        myGroceryListManager.removeItem("Pepsi cola");
         myGroceryListManager.displayList();
-        myGroceryListManager.checkItem("Chicken");
-        myGroceryListManager.checkItem("Tomato");
+        myGroceryListManager.checkItem("Chicken nugget");
+        myGroceryListManager.checkItem("Tomato ketchup");
     }
 }
