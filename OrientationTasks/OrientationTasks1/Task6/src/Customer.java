f625acb34f15ad2fd39d3563a891c3200e19fbf2
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-09-01 11:08:24 +0300

    Cleanup and fix with order

---
+++ OrientationTasks/OrientationTasks1/Task6/src/Customer.java
@@ -0,0 +1,29 @@
+public class Customer {
+    private static int nextId = 1;
+    private final int id;
+    private final long queueStartTime;
+    private long serviceStartTime;
+    private long serviceEndTime;
+
+
+    public Customer() {
+        this.id = nextId++ ;
+        this.queueStartTime = System.currentTimeMillis();
+    }
+
+    //Methods to get values out of object
+    public long getQueueStartTime(){ return queueStartTime; }
+    public long getServiceStartTime(){ return serviceStartTime; }
+    public long getServiceEndTime(){ return serviceEndTime; }
+    public int getId(){ return id; }
+
+    //calculations
+    public long getTimeSpentInQueue(){return serviceStartTime - queueStartTime; }
+    public long getTimeSpentInService(){return serviceEndTime - serviceStartTime; }
+    public long getTimeToRespond(){return serviceEndTime - queueStartTime; }
+
+    //Methods to set values of object
+    public void setServiceStartTime(){ this.serviceStartTime = System.currentTimeMillis(); }
+    public void setServiceEndTime(){ this.serviceEndTime = System.currentTimeMillis(); }
+
+}
--- OrientationTasks/OrientationTasks1/Task6/src/TestMainTask6.java
+++ OrientationTasks/OrientationTasks1/Task6/src/TestMainTask6.java
@@ -10,7 +10,7 @@ public class TestMainTask6 {
             do {
                 Scanner input = new Scanner(System.in);
                 try {
-                    System.out.println("How many loops of simulation? : ");
+                    System.out.print("How many loops of simulation? : ");
                     loops = input.nextInt();
                     check = true;
                 } catch (Exception e) {
@@ -24,7 +24,7 @@ public class TestMainTask6 {
                 System.out.println("Invalid input!");
             }
         }
-
+        System.out.println("-----------------------------------------------");
         ServicePoint2 servicePoint = new ServicePoint2();
         CustomerGenerator2 generator = new CustomerGenerator2();
         for (int loop = 1; loop < loops+1; loop++) {
@@ -32,8 +32,9 @@ public class TestMainTask6 {
             servicePoint.serve(loop);
         }
         System.out.println("Total:");
-        System.out.println("Total waiting time: " + servicePoint.getTotalWaitingTime() + "ms| Average: " + servicePoint.getTotalWaitingTime()/(5*5) + "ms" );
-        System.out.println("Total service time: " + servicePoint.getTotalServiceTime() + "ms| Average: " + servicePoint.getTotalServiceTime()/(5*5) + "ms");
-        System.out.println("Total response time: " + servicePoint.getTotalResponseTime() + "ms| Average: " + servicePoint.getTotalResponseTime()/(5*5) + "ms");
+        System.out.println("Total waiting time: " + servicePoint.getTotalWaitingTime() + "ms| Average: " + servicePoint.getTotalWaitingTime()/(5L *loops) + "ms" );
+        System.out.println("Total service time: " + servicePoint.getTotalServiceTime() + "ms| Average: " + servicePoint.getTotalServiceTime()/(5L *loops) + "ms");
+        System.out.println("Total response time: " + servicePoint.getTotalResponseTime() + "ms| Average: " + servicePoint.getTotalResponseTime()/(5L *loops) + "ms");
+        System.out.println("--------------------------------------------");
     }
 }
