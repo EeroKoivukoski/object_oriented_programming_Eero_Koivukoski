e5b97e063cb4faa99b91d525bf69abe490f4f594
EeroKoivukoski <eero.koivukoski@metropolia.fi>
2025-09-08 20:11:29 +0300

    Update and rename SimulationTest.java to TestMain.java

--- OrientationTasks/OrientationTasks2/TaskO2.2/src/Main/TestMain.java
+++ OrientationTasks/OrientationTasks2/TaskO2.2/src/Main/TestMain.java
@@ -3,7 +3,7 @@ package Main;
 import EventCode.Event;
 import EventCode.EventList;
 
-public class SimulationTest {
+public class TestMain{
     public static void main(String[] args) {
         EventList eventList = new EventList();
 
